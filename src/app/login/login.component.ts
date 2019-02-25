93671968bae6590d395afd90d2a0c4ec4ae5ec91
ckirschberg@gmail.com <ckirschberg@gmail.com>
2019-02-25 14:47:19 +0100

    reactive forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  loginForm: FormGroup;
+  // submitted: boolean = false;
+  
+  // DI - Dependency injection - 
+  constructor(private fb: FormBuilder) {
   }
 
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    // this.submitted = true;
+    if (this.loginForm.valid) {
+      // this.submitted = false;
+      console.log("Form is valid");
+      // send information to the server
+    } else {
+      // display errors
+      console.log("Form is not valid");
+    }
+
+    console.log(this.loginForm);
+  }
 }
