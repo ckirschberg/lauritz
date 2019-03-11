1cd27cddf146cc4b3f3a354142ab52c354944361
ckirschberg@gmail.com <ckirschberg@gmail.com>
2019-03-11 10:43:23 +0100

    material login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { MatDividerModule, MatCardModule, MatSnackBarModule, MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatGridListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,7 @@ import {MatInputModule} from '@angular/material/input';
     AppRoutingModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatFormFieldModule, MatInputModule
+    MatGridListModule, MatMenuModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSnackBarModule, MatCardModule, MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,37 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {MatSnackBar} from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login', // name of component
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  // submitted: boolean = false;
-  
-  // DI - Dependency injection - 
-  constructor(private fb: FormBuilder) {
+
+  // DI - Dependency injection
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar, 
+    private router: Router) {
   }
 
   ngOnInit() {
-    this.loginForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(3)]],
-      password: ['', Validators.required]
-    });
+    this.loginForm = this.fb.group(
+      {
+        username: ['', [Validators.required, Validators.minLength(3)]], // multiple validators
+        password: ['', Validators.required] // Single validator
+      }
+    )
   }
 
-  onSubmit() {
-    // this.submitted = true;
+  onSubmit() : void {
+    this.snackBar.open('One second, logging in..', 'Close', {
+      duration: 2000,
+    });
+
+    
+    console.log(this.loginForm);
+
     if (this.loginForm.valid) {
-      // this.submitted = false;
-      console.log("Form is valid");
-      // send information to the server
-    } else {
-      // display errors
-      console.log("Form is not valid");
+      // Send the data to the server to verify the user login
+      // navigate after successful login.
     }
 
-    console.log(this.loginForm);
   }
 }
