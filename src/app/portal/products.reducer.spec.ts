04a0203b645ef2f94b0b8c7ea3708425d6061236
ckirschberg@gmail.com <ckirschberg@gmail.com>
2019-04-08 16:34:16 +0200

    redux

--- src/app/portal/products.reducer.spec.ts
+++ src/app/portal/products.reducer.spec.ts
@@ -1,7 +1,9 @@
+import { ProductState } from './../store';
 import { TempDataService } from './../services/temp-data.service';
 var deepFreeze = require('deep-freeze');
 import { productsReducer } from './products.reducer';
 import * as types from './product.actions';
+import { Gender } from '../entities/user';
 
 describe('products reducer', () => {
  
@@ -21,5 +23,25 @@ describe('products reducer', () => {
 
   // Create a test to check that your create functionality works AND
   // does not make state mutations
+  it('should add a new product', () => {
+    let stateBefore = {products: []} as ProductState; 
+    deepFreeze(stateBefore);
 
+    let product = {_id: '1', user: 
+        { _id:'1', username: 'abc', email: 'abc@kea.dk', 
+        firstname: 'Asger', lastname: 'Poulsen', phone: '12121212', 
+        gender: Gender.Male, birthDate: new Date(1985, 2, 2),
+      profileImage: 'https://amp.businessinsider.com/images/5899ffcf6e09a897008b5c04-750-750.jpg'},
+        
+      name: 'Hair Brush', description: 'test bla bla', 
+      startingPrice: 100, minimumBid: 10, 
+      images: ['https://cdn.shopify.com/s/files/1/0066/0052/files/Hair_brush_guide_6.jpg'], dateCreated: new Date(2019,2,18), 
+      endDate: new Date(2019,3,18), bids: [], location: 'Copenhagen'
+    }
+
+    let stateAfter = {products: [product]};
+
+    let response = productsReducer(stateBefore, {type: types.ProductActions.CREATE_PRODUCT, payload: product});
+    expect(stateAfter).toEqual(response);
+  });
 });
